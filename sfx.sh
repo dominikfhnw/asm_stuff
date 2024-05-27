563535a0be3f42b821233add8ac2dff083895b4c
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-05-27 18:39:58 +0200

    selfextractor

---
+++ sfx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+IN=$1
+OUT=${2:-${IN}-sfx}
+
+printf "\x1f\x8b\x08\x08" > "$OUT"
+cat <<'EOF' >> "$OUT"
+ 2>&-
+zcat<$0>i 2>&-
+exec /*b/ld* ./i
+EOF
+printf "\0" >> "$OUT"
+zopfli --i1000 --deflate -c "$IN" >> "$OUT"
+#truncate -s -1 "$OUT"
+chmod +x "$OUT"
+ls -l "$IN" "$OUT"
