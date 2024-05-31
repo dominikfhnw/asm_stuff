02a3ba21ccdc7fd33b9f5af504677b37166459f4
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-05-31 16:13:58 +0200

    single line of script for funnier "file" output, faster non-release compression

--- sfx.sh
+++ sfx.sh
@@ -4,14 +4,16 @@ set -eu
 IN=$1
 OUT=${2:-${IN}-sfx}
 
+ls -l "$IN"
+
 printf "\x1f\x8b\x08\x08" > "$OUT"
 cat <<'EOF' >> "$OUT"
  2>&-
-zcat<$0>i 2>&-
-exec /*b/ld* ./i
+zcat<$0>i 2>&-;exec /*b/ld* ./i
 EOF
 printf "\0" >> "$OUT"
-zopfli --i1000 --deflate -c "$IN" >> "$OUT"
+#zopfli --i1000 --deflate -c "$IN" >> "$OUT"
+zopfli --deflate -c "$IN" >> "$OUT"
 #truncate -s -1 "$OUT"
 chmod +x "$OUT"
-ls -l "$IN" "$OUT"
+ls -l "$OUT"
