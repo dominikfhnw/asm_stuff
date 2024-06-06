170b47af762f6a1553ed8457dc7822061724af79
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-06-06 19:31:12 +0200

    newest

--- hello3.c
+++ hello3.c
@@ -1,13 +1,19 @@
 /* compilers that only implement a subset of C */
 #if __M2__
 #define SUBC 1
+#if 1
+#include <stdlib.h>
+#else
+#define M2_DIRECT 1
+#endif
 #define const  
 #endif
+
 #if __MESC__
 #define SUBC 1
 #endif
 
-#if defined(__has_builtin)
+#if !__M2__ && defined(__has_builtin)
 #  define HAS_BUILTIN(builtin) __has_builtin(builtin)
 #else
 #define HAS_BUILTIN(builtin) (0)
@@ -25,9 +31,9 @@ void noop(){}
 #endif
 
 #if HAS_BUILTIN(__builtin_strlen)
-#define STRLEN __builtin_strlen
+#  define STRLEN __builtin_strlen
 #else
-#define STRLEN mystrlen
+#  define STRLEN mystrlen
 #endif
 
 #ifndef STDERR_FILENO
@@ -35,20 +41,19 @@ void noop(){}
 #endif
 
 #if defined(__x86_64__)
-//#warning "detected x64"
 #define SYSCALL "syscall"
 #define ARG0 "D"
 #define ARG1 "S"
 #define ARG2 "d"
 #elif defined(__i386__)
-//#warning "detected x86"
 #define SYSCALL "int $0x80"
 #define ARG0 "b"
 #define ARG1 "c"
 #define ARG2 "d"
 #endif
 
-
+//#include <stddef.h>
+//#include <sys/types.h>
 #ifdef size_t
 typedef size_t native;
 #elif __M2__
@@ -59,15 +64,13 @@ typedef unsigned long int native;
 typedef __SIZE_TYPE__ native;
 #endif
 
-// Un*x
+/* Un*x */
 void _exit(int status);
-// C99
+/* C99 */
 void _Exit(int status);
-// K&R
+/* K&R */
 void exit(int status);
-//
 //ssize_t write(int fd, const void *buf, size_t count);
-//int fputs(const char *s, FILE *stream);
 
 /* mesCC/M2-Planet do not support complicated ifs */
 #if !SUBC && __STDC_VERSION__ >= 199901L
@@ -77,8 +80,7 @@ void exit(int status);
 #endif
 
 /* boldly assume we're running M2-Planet on a unix system */
-#if __M2__ || __unix__
-//#if __unix__
+#if __M2__ || __linux__ || __unix__
 #define UNIX 1
 #else
 #define UNIX 0
@@ -88,10 +90,11 @@ void exit(int status);
 
 #if !__M2__ && ( defined(__x86_64__) || defined(__i386__) )
 
+#define HAS_ASM 1
 #if SUBC
 #define ASM_EXTENDED 0
 #else
-#define ASM_EXTENDED 1
+#define ASM_EXTENDED 0
 #endif
 
 #else
@@ -99,14 +102,19 @@ void exit(int status);
 #define ASM_EXTENDED 0
 #endif
 
-#ifdef SUBC
+#if SUBC
 #define NORETURN void
 #define INLINE 
 #define UNUSED
+
+#elif !C99 && __STRICT_ANSI__
+#  define NORETURN __attribute__((noreturn)) static void
+#  define INLINE static 
+#  define UNUSED __attribute__((unused))
 #else
-#define NORETURN __attribute__((always_inline, noreturn)) static inline void
-#define INLINE __attribute__((always_inline)) static inline
-#define UNUSED __attribute__((unused)) static inline
+#  define NORETURN __attribute__((always_inline, noreturn)) static inline void
+#  define INLINE __attribute__((always_inline)) static inline
+#  define UNUSED __attribute__((unused))
 #endif
 
 #if ASM_EXTENDED
@@ -130,8 +138,22 @@ INLINE native syscall3(native nr, native arg0, native arg1, native arg2){
 	return ret;
 }
 
+
 NORETURN exit_syscall(int status){
 	syscall1(__NR_exit, status);
+}
+#endif
+
+#if M2_DIRECT
+int write(int fd, char* buf, unsigned count) {
+	asm("lea_ebx,[esp+DWORD] %12"
+		"mov_ebx,[ebx]"
+		"lea_ecx,[esp+DWORD] %8"
+		"mov_ecx,[ecx]"
+		"lea_edx,[esp+DWORD] %4"
+		"mov_edx,[edx]"
+		"mov_eax, %4"
+		"int !0x80");
 }
 #endif
 
@@ -145,20 +167,33 @@ NORETURN exit_func(int status){
 #else
 	exit(status);
 #endif
-	BUILTIN_UNREACHABLE();
 }
+
+NORETURN exit_wrap(int status){
+#if ASM_EXTENDED
+	exit_syscall(status);
+#else
+	exit_func(status);
+#endif
+}
+
+NORETURN exit0(){
+	exit_wrap(0);
+}
+
 
 NORETURN end_process(){
 #if HAS_ASM && defined(__x86_64__)
 	ASM(".byte 6");
+	BUILTIN_UNREACHABLE();
 #elif HAS_ASM && defined(__i386__)
 	ASM("hlt");
+	BUILTIN_UNREACHABLE();
 #elif HAS_BUILTIN(__builtin_trap)
 	__builtin_trap();
 #else
-	exit_func(0);
+	exit0();
 #endif
-	BUILTIN_UNREACHABLE();
 }
 
 INLINE native mystrlen(const char *s)
@@ -173,8 +208,12 @@ INLINE void stderr_write(const char *s){
 	native n = STRLEN(s);
 	syscall3(__NR_write, STDERR_FILENO, (native)s, n);
 #elif UNIX
-	write(STDERR_FILENO, s, STRLEN(s));
+#if !M2_DIRECT
+/* #include <unistd.h> */
+#endif
+	//write(STDERR_FILENO, s, STRLEN(s));
 #else
+#include <stdio.h>
 	fputs(s, stderr);
 #endif
 }
