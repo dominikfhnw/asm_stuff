bc0d109dfac33fb8b81552db669d1c8e51b30790
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-06-08 13:49:07 +0200

    us cpp/perl/tsort for creating a kaem script that links in the right order

--- dep.pl
+++ dep.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-use v5.39;
+
 my $file = $ARGV[0];
 my $name = $ARGV[1] || 'root';
 
@@ -9,24 +9,15 @@ sub recurse {
 	my $dot = shift;
 	my $name = shift;
 	my $start = shift;
-	my $spc = "\t"x$dot;
-	#say STDERR "${spc}RECURSE $dot $start $name";
 
 	open my $fh, '<', $file || die;
 	while(<$fh>){
 		/^(\.+) (.*)/;
 		next if $. < $start;
 		my $f = length $1;
-		#say STDERR "${spc}LOOP $dot $. $f $2";
 		if($f == 1+$dot){
-			my $newname = $2;
-			#my $nb = $newname;
-			#$nb =~ s!.*/!!;
-			#my $basename = $name;
-			#$basename =~ s!.*/!!;
-			#say STDERR "${spc}$nb";
-			print "$newname $name\n";
-			recurse(1+$dot, $newname, 1+$.);
+			print "$2 $name\n";
+			recurse(1+$dot, $2, 1+$.);
 		}elsif($f > $dot){
 		}else{
 			return;
