ebeaf9868cb6c2f1e3f3441ff83070e1b2153f00
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2024-06-24 12:01:25 +0200

    headers

---
+++ header/rwx.asm
@@ -0,0 +1,64 @@
+true : ;out=$(basename $0 .asm);yasm -f bin -o $out $0 && ls -l $out && chmod +x $out && objdas $out && strace -i ./$out; echo ret $?; exit
+
+;; true.asm: Copyright (C) 2001 Brian Raiter <breadbox@muppetlabs.com>
+;; Licensed under the terms of the GNU General Public License, either
+;; version 2 or (at your option) any later version.
+;;
+;; To build:
+;;	nasm -f bin -o true true.asm && chmod +x true
+;;	ln true false
+%define SYSCALL int 0x80
+
+BITS 32
+START equ _start
+		org	0x68490000
+
+		db	0x7F, "ELF"
+		dd	1
+		dd	0
+		dd	$$			; vaddr
+		dw	2
+		dw	3
+		dd	START -2		; garbage/filesz
+		dd	START -2		; start/memsz
+_start:
+		dd	4
+mov	ebx, $$
+;; initialize base pointer
+mov	ebp, esp
+
+add	al, 125
+;jmp	_start2
+times $$-$+41	db	0x90
+		db	169
+		dw	0x20
+		dw	1
+
+_start2:
+pop	edx
+dec	edx
+SYSCALL
+
+%if 0
+push 29
+pop eax
+SYSCALL
+%endif
+%if 0
+mov	edx, 162
+;; unneeded if top of stack < 1e9
+push	edx
+push	edx
+mov	ebx,esp
+
+xor	ecx,ecx
+; ecx = 0
+; edx = 162
+; ebx = esp
+_loop:
+mov	eax,edx
+SYSCALL
+jmp	_loop
+
+%endif
+
---
+++ header/teensy.asm
@@ -0,0 +1,31 @@
+true : ;out=$(basename $0 .asm);yasm -f bin -o $out $0 && ls -l $out && chmod +x $out && objdas $out && ./$out; echo ret $?; exit
+
+;; true.asm: Copyright (C) 2001 Brian Raiter <breadbox@muppetlabs.com>
+;; Licensed under the terms of the GNU General Public License, either
+;; version 2 or (at your option) any later version.
+;;
+;; To build:
+;;	nasm -f bin -o true true.asm && chmod +x true
+;;	ln true false
+
+BITS 32
+START equ _start
+		org	0x68400000
+
+		db	0x7F, "ELF"
+		dd	1
+		dd	0
+		dd	$$			; vaddr
+		dw	2
+		dw	3
+		dd	START 			; garbage/filesz
+		dd	START 			; start/memsz
+		dd	4
+_start:
+
+times 10-$+_start db	0x90
+		;db	169
+		dw	0x20
+		db	1
+
+_start2:
